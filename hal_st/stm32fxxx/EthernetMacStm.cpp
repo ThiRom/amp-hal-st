278d9b8e369181423f365898ec30bed8d8371c8f
Roman Thiel <roman.thiel@philips.com>
2025-05-23 08:34:22 +0000

    Extended init (HAL like)

--- hal_st/stm32fxxx/EthernetMacStm.cpp
+++ hal_st/stm32fxxx/EthernetMacStm.cpp
@@ -10,6 +10,7 @@
 #define ETH_SEGMENT_SIZE_DEFAULT        0x218U
 #define ETH_DMACTCR_TPBL_32PBL          ((uint32_t)0x00200000)   /* Transmit Programmable Burst Length 32 */
 #define ETH_DMACRCR_RPBL_32PBL          ((uint32_t)0x00200000)   /* Receive Programmable Burst Length 32 */
+#define ETH_MACCR_MASK                  0xFFFB7F7CU
 
 namespace hal
 {
@@ -23,25 +24,92 @@ namespace hal
         , receiveDescriptors(*this)
         , sendDescriptors(*this)
     {
+        uint32_t tickstart;
+        uint32_t hclk;
+        uint32_t tmpreg;
+
+        EnableClockEthernet(0);
+
+        // Select RMII Mode
+        MODIFY_REG(SBS->PMCR, SBS_PMCR_ETH_SEL_PHY, (uint32_t)(SBS_ETH_RMII));
+        // Dummy read to sync with ETH
+        (void)SBS->PMCR;
+
+        // Get the ETHERNET MACMDIOAR value
+        tmpreg = peripheralEthernet[0]->MACMDIOAR;
+        // Clear CSR Clock Range bits
+        tmpreg &= ~ETH_MACMDIOAR_CR;
+        // Get hclk frequency value
+        hclk = HAL_RCC_GetHCLKFreq();
+        // Set CR bits depending on hclk value
+        if (hclk < 35000000U)
+        {
+            // CSR Clock Range between 0-35 MHz
+            tmpreg |= ETH_MACMDIOAR_CR_DIV16;
+        }
+        else if (hclk < 60000000U)
+        {
+            // CSR Clock Range between 35-60 MHz
+            tmpreg |= ETH_MACMDIOAR_CR_DIV26;
+        }
+        else if (hclk < 100000000U)
+        {
+            // CSR Clock Range between 60-100 MHz
+            tmpreg |= ETH_MACMDIOAR_CR_DIV42;
+        }
+        else if (hclk < 150000000U)
+        {
+            // CSR Clock Range between 100-150 MHz
+            tmpreg |= ETH_MACMDIOAR_CR_DIV62;
+        }
+        else if (hclk < 250000000U)
+        {
+            // CSR Clock Range between 150-250 MHz
+            tmpreg |= ETH_MACMDIOAR_CR_DIV102;
+        }
+        else // (hclk >= 250000000U)
+        {
+            // CSR Clock >= 250 MHz
+            tmpreg |= ETH_MACMDIOAR_CR_DIV124;
+        }
+        // Configure the CSR Clock Range
+        peripheralEthernet[0]->MACMDIOAR |= tmpreg;
+
+        // MAC LPI 1US Tic Counter Configuration
+        peripheralEthernet[0]->MAC1USTCR |= (((uint32_t)HAL_RCC_GetHCLKFreq() / ETH_MAC_US_TICK) - 1U);
+
+        // Set MAC address
         peripheralEthernet[0]->MACA0LR = reinterpret_cast<const uint32_t*>(macAddress.data())[0];
         peripheralEthernet[0]->MACA0HR = reinterpret_cast<const uint32_t*>(macAddress.data())[1] & 0xffff;
 
-        peripheralEthernet[0]->MACCR = ((linkSpeed == LinkSpeed::fullDuplex100MHz || linkSpeed == LinkSpeed::halfDuplex100MHz) ? ETH_MACCR_FES : 0) | ((linkSpeed == LinkSpeed::fullDuplex100MHz || linkSpeed == LinkSpeed::fullDuplex10MHz) ? ETH_MACCR_DM : 0) | ETH_MACCR_TE | ETH_MACCR_RE;
+        //Set default MAC settings like in HAL_ETH see ETH_MACDMAConfig()
+        peripheralEthernet[0]->MACCR |= ((linkSpeed == LinkSpeed::fullDuplex100MHz || linkSpeed == LinkSpeed::halfDuplex100MHz) ? ETH_MACCR_FES : 0) |
+                                        ((linkSpeed == LinkSpeed::fullDuplex100MHz || linkSpeed == LinkSpeed::fullDuplex10MHz) ? ETH_MACCR_DM : 0) |
+                                        ETH_MACCR_SARC_REPADDR0 | ETH_MACCR_CST | ETH_MACCR_IPC | ETH_MACCR_JD | ETH_MACCR_TE | ETH_MACCR_RE;
+
+        //Set Transmit Store and Forward
+        peripheralEthernet[0]->MTLTQOMR |= ETH_MTLTQOMR_TSF;
 
         //Set default DMA settings like in HAL_ETH see ETH_MACDMAConfig()
-        peripheralEthernet[0]->DMASBMR = ETH_DMASBMR_AAL;
-        peripheralEthernet[0]->DMACCR = ETH_SEGMENT_SIZE_DEFAULT;
-        peripheralEthernet[0]->DMACTCR = ETH_DMACTCR_TPBL_32PBL;
-        peripheralEthernet[0]->DMACRCR = ETH_DMACRCR_RPBL_32PBL;
+        peripheralEthernet[0]->DMASBMR |= ETH_DMASBMR_AAL;
+        peripheralEthernet[0]->DMACCR |= ETH_SEGMENT_SIZE_DEFAULT | ETH_DMACCR_DSL_64BIT;
+        peripheralEthernet[0]->DMACTCR |= ETH_DMACTCR_TPBL_32PBL;
+        peripheralEthernet[0]->DMACRCR |= ETH_DMACRCR_RPBL_32PBL;
 
         //Enable interrupts
-        peripheralEthernet[0]->DMACIER = ETH_DMACIER_TIE | ETH_DMACIER_RIE | ETH_DMACIER_RSE | ETH_DMACIER_FBEE | ETH_DMACIER_AIE | ETH_DMACIER_NIE;
+        peripheralEthernet[0]->DMACIER |= ETH_DMACIER_TIE | ETH_DMACIER_RIE | ETH_DMACIER_RSE | ETH_DMACIER_FBEE | ETH_DMACIER_AIE | ETH_DMACIER_NIE;
+
+        // Disable Rx MMC Interrupts (mask register -> 1 = OFF)
+        peripheralEthernet[0]->MMCRIMR |= ETH_MMCRIMR_RXLPITRCIM | ETH_MMCRIMR_RXLPIUSCIM | ETH_MMCRIMR_RXUCGPIM | ETH_MMCRIMR_RXALGNERPIM | ETH_MMCRIMR_RXCRCERPIM;
+
+        // Disable Tx MMC Interrupts (mask register -> 1 = OFF)
+        peripheralEthernet[0]->MMCTIMR |= ETH_MMCTIMR_TXLPITRCIM | ETH_MMCTIMR_TXLPIUSCIM | ETH_MMCTIMR_TXGPKTIM | ETH_MMCTIMR_TXMCOLGPIM | ETH_MMCTIMR_TXSCOLGPIM;
     }
 
     EthernetMacStm::~EthernetMacStm()
     {
         ResetDma();
-        peripheralEthernet[0]->MACCR = 0;
+        MODIFY_REG(peripheralEthernet[0]->MACCR, ETH_MACCR_MASK, 0);
     }
 
     void EthernetMacStm::SendBuffer(infra::ConstByteRange data, bool last)
